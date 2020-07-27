1a3684b512cb2cd4e3b4e7705fd10c57efad0038
Arseniy Kravtsov <ars.kravtsov@yandex.ru>
2020-07-27 11:28:39 +0500

    Урок№6

---
+++ lesson_6/bookparser/runner.py
@@ -0,0 +1,18 @@
+from scrapy.crawler import CrawlerProcess
+from scrapy.settings import Settings
+
+from lesson_6.bookparser import settings
+from lesson_6.bookparser.spiders.book24 import Book24Spider
+from lesson_6.bookparser.spiders.labirint import LabirintSpider
+
+
+if __name__ == '__main__':
+
+    crawler_settings = Settings()
+    crawler_settings.setmodule(settings)
+
+    process = CrawlerProcess(settings=crawler_settings)
+    process.crawl(LabirintSpider)
+    # process.crawl(Book24Spider)
+
+    process.start()
